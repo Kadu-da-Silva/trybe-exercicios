1c6e2b54f7c625104e02c937d13f576b8665b886
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2022-11-13 21:43:51 -0300

    exercício/dia03

--- fundamentos/secao-04-intro-js/dia-02-array-loop.for/exercicio-bonus.js
+++ fundamentos/secao-04-intro-js/dia-02-array-loop.for/exercicio-bonus.js
@@ -40,3 +40,13 @@ for (let i = 0; i < numbers.length; i += 1){
 }
 
 console.log(array);
+
+
+//observações
+const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
